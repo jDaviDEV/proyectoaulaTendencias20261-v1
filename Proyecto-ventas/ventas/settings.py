bb73a2b5b27d4c82c7d2ca8287f29c8eff30a83c
INGJEISSON <jeisim18@gmail.com>
2026-03-28 11:50:22 -0500

    Update settings.py

--- Proyecto-ventas/ventas/settings.py
+++ Proyecto-ventas/ventas/settings.py
@@ -81,7 +81,12 @@ WSGI_APPLICATION = 'ventas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
